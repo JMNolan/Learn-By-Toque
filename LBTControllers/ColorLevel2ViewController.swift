92001e4a2724d28d7f3b3c37a91686fa00c34244
John Nolan <johnstaylownolan@gmail.com>
2018-12-11 02:09:05 -0600

    Keeps same color from being placed on two buttons and buttons from appearing with no label. Colors level 2 is now fully functional.

--- LBTControllers/ColorLevel2ViewController.swift
+++ LBTControllers/ColorLevel2ViewController.swift
@@ -22,11 +22,13 @@ class ColorLevel2ViewController: UIViewController {
     var correctColor: LBTModel.color?
     var madeOfColors: [String] = []
     var correctButtons: [ColorsButton] = []
+    var correctButtonsPressed: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setCorrectColor()
         generateNewQuestion()
+        print("the color is made of \(madeOfColors)")
+        print("the correct buttons are \(correctButtons[0]) and \(correctButtons[1])")
     }
     
     //MARK: IBActions
@@ -57,20 +59,27 @@ class ColorLevel2ViewController: UIViewController {
     
     //picks a random color from an array of all colors in the app and sets the question label accordingly
     func setCorrectColor() {
+        madeOfColors = []
         correctColor = ColorsLibrary.secondaryColors.randomElement()!
         let color = correctColor?.color
+        print("the correct color is \(String(describing: correctColor?.color))")
         madeOfColors.append((correctColor?.madeOf[0])!)
+        print("added \(String(describing: madeOfColors[0]))")
         madeOfColors.append((correctColor?.madeOf[1])!)
-        questionLabel.text = "Which color is \(color!)?"
+        print("added \(String(describing: madeOfColors[1]))")
+        questionLabel.text = "Which colors make \(color!)?"
     }
     
     //checks each button to see if it is the "correct" button. then randomly sets a color to that button if its not the "correct" one
     func setButtonColors() {
         var colors = pullColorsFromArray(colorObjects: ColorsLibrary.allColors)
+        colors.removeAll {$0 == madeOfColors[0]}
+        colors.removeAll {$0 == madeOfColors[1]}
+        print("The wrong colors are \(colors)")
         if correctButtons.contains(button1) {
             button1.isCorrectButton = true
             button1.color = madeOfColors.randomElement()
-            colors.removeAll {$0 == button1.color}
+            madeOfColors.removeAll {$0 == button1.color}
             button1.setTitle(button1.color, for: .normal)
         } else {
             button1.isCorrectButton = false
@@ -81,8 +90,8 @@ class ColorLevel2ViewController: UIViewController {
         
         if correctButtons.contains(button2) {
             button2.isCorrectButton = true
-            button2.color = correctColor?.color
-            colors.removeAll {$0 == button2.color}
+            button2.color = madeOfColors.randomElement()
+            madeOfColors.removeAll {$0 == button2.color}
             button2.setTitle(button2.color, for: .normal)
         } else {
             button2.isCorrectButton = false
@@ -93,8 +102,8 @@ class ColorLevel2ViewController: UIViewController {
         
         if correctButtons.contains(button3) {
             button3.isCorrectButton = true
-            button3.color = correctColor?.color
-            colors.removeAll {$0 == button3.color}
+            button3.color = madeOfColors.randomElement()
+            madeOfColors.removeAll {$0 == button3.color}
             button3.setTitle(button3.color, for: .normal)
         } else {
             button3.isCorrectButton = false
@@ -105,8 +114,8 @@ class ColorLevel2ViewController: UIViewController {
         
         if correctButtons.contains(button4) {
             button4.isCorrectButton = true
-            button4.color = correctColor?.color
-            colors.removeAll {$0 == button4.color}
+            button4.color = madeOfColors.randomElement()
+            madeOfColors.removeAll {$0 == button4.color}
             button4.setTitle(button4.color, for: .normal)
         } else {
             button4.isCorrectButton = false
@@ -131,26 +140,70 @@ class ColorLevel2ViewController: UIViewController {
     }
     
     func buttonPressed(button: ColorsButton){
+        
+        if correctButtons.contains(button) {
+            correctButtonPressed(button: button)
+        } else {
+            wrongButtonPresed()
+        }
+    }
+    
+    func correctButtonPressed(button: ColorsButton) {
         var alertTitle: String!
         var alertMessage: String!
-        if button.isCorrectButton! {
+        var next: UIAlertAction!
+        correctButtonsPressed += 1
+        if correctButtonsPressed < 2 {
             alertTitle = "Great job!"
-            alertMessage = "Would you like to try another?"
+            alertMessage = "One more to go."
+            next = UIAlertAction(title: "Next", style: .default, handler: { action in
+                self.dismiss(animated: true, completion: nil)
+            })
+            button.isEnabled = false
         } else {
-            alertTitle = "Close!"
+            alertTitle = "You got it!"
             alertMessage = "Would you like to try another?"
+            next = UIAlertAction(title: "Next", style: .default, handler: { action in
+                self.generateNewQuestion()
+            })
+            enableAllButtons()
+            madeOfColors = []
+            correctButtonsPressed = 0
+            correctButtons = []
         }
-        let tryAgain = UIAlertAction(title: "Try Another", style: .default, handler: { action in
-            self.generateNewQuestion()
-        })
+        
         let backToMainMenu = UIAlertAction(title: "No thanks", style: .default, handler: { action in
             //TODO: call rewind to main menu
             self.dismiss(animated: true, completion: nil)
         })
         let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .actionSheet)
-        alert.addAction(tryAgain)
+        alert.addAction(next)
         alert.addAction(backToMainMenu)
         present(alert, animated: true, completion: nil)
     }
     
+    func wrongButtonPresed() {
+        let alertTitle = "Oops!"
+        let alertMessage = "Try Again?"
+        let next = UIAlertAction(title: "Next", style: .default, handler: { action in
+            self.generateNewQuestion()
+        })
+        
+        let backToMainMenu = UIAlertAction(title: "Main Menu", style: .default, handler: { action in
+            //TODO: create rewind to main menu
+            self.dismiss(animated: true, completion: nil)
+        })
+        
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(next)
+        alert.addAction(backToMainMenu)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func enableAllButtons() {
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+    }
 }
